fcd05affabc63f5491e9bf6af76e22cc73d18e24
strolger <louis.strolger@wku.edu>
2018-10-05 14:44:07 -0400

    modified:   MCMC_SFD.py 	modified:   temporary.png

--- analysis/scripts/MCMC_SFD.py
+++ analysis/scripts/MCMC_SFD.py
@@ -33,9 +33,9 @@ def dtdfit(time,*p):
 
 def lnprior(p):
     ff, m, w, k, lnf = p
-    if abs(m) < 0.1 or abs(w) < 0.1 or abs(k) < 0.1:
-        return -np.inf
-    elif 0.0 < ff < 1.0 and -2000.0 < m < 2000.0 and 0.1 < w < 100.0 and -500.0 < k < 500.0 and -4.0 < lnf < -0.1:
+    ## if abs(m) < 10. or abs(w) < 10. or abs(k) < 10.:
+    ##     return -np.inf
+    if 0.0 < ff < 1.0 and -2000.0 < m < 2000.0 and 0.01 < w < 100.0 and -500.0 < k < 500.0 and -4.0 < lnf < -0.1:
         return 0.0
     else:
         return -np.inf
