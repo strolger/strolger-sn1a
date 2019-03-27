27451ad65569f5d70b6296ece7791a8bd1e29b46
strolger <louis.strolger@wku.edu>
2019-03-27 16:46:54 -0400

    modified:   junk.png 	modified:   what_exp_pwr.py

--- analysis/junk/what_exp_pwr.py
+++ analysis/junk/what_exp_pwr.py
@@ -13,18 +13,38 @@ if __name__=='__main__':
     p_pwr = (-1., 1.)
     yy = rz.powerdtd(tt, *p_pwr)#, normed=False)
 
+
+
+    zz = yy*0.0
+    id1 = where(tt < 0.5)
+    zz[id1]=sum(yy[id1])
+    id2 = where(tt >= 0.5)
+    zz[id2]=sum(yy[id2])
+
+    
     ax=subplot(111)
     ax.plot(tt, yy, 'r--')
-    #ax.set_xlim(0.04,14)
-
-
-    p0=(2,2,1)
+    ax.plot(tt,zz, 'b--')
+    p0 = (1,1,1)
     popt,pcov=curve_fit(rz.dtdfunc, tt, yy, p0=p0)
     ax.plot(tt, rz.dtdfunc(tt,*popt), '-',color='orange')
     print(popt)
-    p1 = (0.15, 2, -0.7)
-    ax.plot(tt, rz.dtdfunc(tt,*p1), 'r-')
+    p1=(-1258, 59, 248)
+
+    yy = rz.dtdfunc(tt,*p1)
+
+    zz = yy*0.0
+    id1 = where(tt < 0.5)
+    zz[id1]=sum(yy[id1])
+    id2 = where(tt >= 0.5)
+    zz[id2]=sum(yy[id2])
+
+    ax.plot(tt,yy , 'r-')
+    ax.plot(tt,zz , 'b-')
+
+
     
+    #ax.set_xlim(0.04,14)
     savefig('junk.png')
     
               
