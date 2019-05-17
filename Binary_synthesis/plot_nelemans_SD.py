b568af1026a3fcba08dd0e5ef672459f17976c35
strolger <louis.strolger@wku.edu>
2019-05-17 15:12:20 -0400

    new file:   Binary_synthesis/figure_pwr.png 	new file:   Binary_synthesis/figure_sd_dd_fits.png 	new file:   Binary_synthesis/maoz_data.txt 	new file:   Binary_synthesis/maoz_data.txt~ 	new file:   Binary_synthesis/maoz_table2.txt 	new file:   Binary_synthesis/nelemans_table2.txt 	new file:   Binary_synthesis/nelemans_table2.txt~ 	modified:   Binary_synthesis/plot_nelemans_SD.py 	new file:   Binary_synthesis/plot_pwr_law.py 	new file:   Binary_synthesis/plot_pwr_law.py~ 	modified:   analysis/junk/junk.png 	modified:   paper/strolger.aux 	modified:   paper/strolger.log 	modified:   paper/strolger.pdf 	modified:   paper/strolger.tex

--- Binary_synthesis/plot_nelemans_SD.py
+++ Binary_synthesis/plot_nelemans_SD.py
@@ -9,9 +9,13 @@ from scipy.optimize import curve_fit
 if __name__=='__main__':
 
 
+    fig= plt.figure()
+    grid = plt.GridSpec(1,3,wspace=0.2)
+
     data = loadtxt('nelemans_table.txt')
-    #data = data[:,:-3]
-    ax=subplot(111)
+
+    ax = fig.add_subplot(grid[0,:-1])
+    ax2 = fig.add_subplot(grid[0,2])
     tt = arange(0,10000,100)
     p0 = [
         [10, 600, 220],
@@ -43,16 +47,42 @@ if __name__=='__main__':
     ax.errorbar(data[:,0], data[:,6]*1e-3, xerr=data[:,1], fmt='o', color=u.my_color(6), label='Ruiter et al. 2009')
     ax.plot(tt,rz.dtdfunc(tt,*p0[ii])*scales[ii], color=u.my_color(6))
 
+
+    ## ax2 =subplot(122)
+    data = loadtxt('nelemans_table2.txt')
+    ax2.errorbar(data[:,0], data[:,2]*1e-3, xerr=data[:,1], fmt='o',color=u.my_color(0), label='Yungelson 2010')
+
+    pa = (-1.0, 1.0)
+    yy = rz.powerdtd(tt, *pa)
+    ax2.plot(tt, yy*2.0, color=u.my_color(2), label=r'$t^{-1}$')
+    p1=(-662, 2200, 1101)
+    ax2.plot(tt, 5*rz.dtdfunc(tt,*p1), '--', color=u.my_color(5), label='best model fit')
+
+
+    ax2.set_xscale('log')
+    ax2.set_yscale('log')
+
+    ax2.set_xlim(100,10000)
+    ## ax2.set_ylim(1e-5, 0.005)
+    ax2.set_ylim(4e-7, 2e-2)
+    ax2.set_xlabel('Delay time (Myr)')
+
+
+    ax2.set_yticks([])
+    ax2.legend(loc=3, fontsize=9,frameon=False)
+    ax2.set_title('DD Models')
+
     
 
     ax.set_xscale('log')
     ax.set_yscale('log')
     ax.set_ylim(4e-7,2e-2)
-    ax.legend(loc=3)
+    ax.legend(loc=3, frameon=True)
 
     ax.set_xlabel('Delay time (Myr)')
     ax.set_ylabel(r'SN Ia yr$^{-1}$ ($10^{10}$ M$_{\odot}$)$^{-1}$')
-    savefig('figure_sd_fits.png')
+    ax.set_title('SD Models')
+    savefig('figure_sd_dd_fits.png')
     
     
     
