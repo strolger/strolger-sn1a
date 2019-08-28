20558d55797cdf3164486d1dd29254eec02bdbf1
strolger <louis.strolger@wku.edu>
2019-08-28 17:31:28 -0400

    modified:   aic_bic.py 	modified:   ../SNuM/SNuM_all.pdf 	modified:   ../../paper/strolger.aux 	modified:   ../../paper/strolger.log 	modified:   ../../paper/strolger.pdf 	deleted:    ../../paper/strolger.synctex.gz 	modified:   ../../paper/strolger.tex

--- analysis/DTD_fits/aic_bic.py
+++ analysis/DTD_fits/aic_bic.py
@@ -18,26 +18,6 @@ warnings.simplefilter("ignore",RuntimeWarning)
 
 
 
-def dtdfit(time,*p):
-    ff, aa, bb, cc = p
-    scale = quad(imf.salpeter,3,8)[0]/quad(imf.salpeter1,0.1,125)[0]
-    scale = scale *0.7**2.*1e4
-    par_model = [0.0134, 2.55, 3.3, 6.1]
-    sfh = rz.csfh_time(time, *par_model)
-    dt = sum(diff(time))/(len(time)-1)
-    p1 = (aa, bb, cc)
-    res = rz.dtdfunc(time, *p1,norm=True)
-    tmp = convolve(sfh, res, 'full')
-    return(ff*tmp[:len(time)]*dt*scale)
-
-def lnlike(p, x, y, yerr):
-    ff, m, w, k, lnf = p
-    p1 = (ff, m, w, k)
-    model = dtdfit(x,*p1)
-    inv_sigma2 = 1.0/(yerr**2 + model**2*np.exp(2*lnf))
-    return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
-
-
 def aic_bic(x,y,ym,nvar):
     ssc = sum((y - ym)**2.)
     aic = 2.*nvar-2.*log(ssc)
@@ -53,27 +33,11 @@ if __name__=='__main__':
     rates = rates[:,:4]
     brates = u.gimme_rebinned_data(rates,verbose=False,splits=arange(0,1.167,0.167).tolist())
     data = deepcopy(rates) #this seems to work better than then actual values
-    #data = deepcopy(rates[rates[:,0].argsort()])
-    if calculate:
-        tt = 13.6-array([ct.cosmotime(x, ho=70) for x in data[:,0]])
-        data[:,0] = tt
-        data = data[argsort(data[:,0])]
 
-        import scipy.optimize as op
-        nll = lambda *args: -lnlike(*args)
-        p0 = (0.05, 3.5, 2.5, 2.5, 0.01)
-        res = op.minimize(nll, [p0[0],p0[1],p0[2],p0[3],log(p0[4])],
-                          bounds=[(0.,1.0),(-2000.,2000.),(0.001,100.),(-500.0,500.0),(-4.,0.)],
-                          args=(data[:,0], data[:,1], data[:,3]))
-
-        p2=res['x'][1:4]
-        frac = res['x'][0]
-        print(p2,frac)
-
-    else:
-        p2 = [-1518.39108091,    51.06020462,    49.98728772]
-        frac = 0.062
-
+    p0 = [-1518, 51,  50]
+    p1 = [-1258, 59, 248]
+    p2 = [-1087, 73, 242]
+    frac = 0.062
 
     age = 13.6
     scale_k = quad(imf.salpeter,3,8)[0]/quad(imf.salpeter1,0.1,125)[0]
@@ -86,62 +50,35 @@ if __name__=='__main__':
     par_model = [0.0134, 2.55, 3.3, 6.1]
 
     sfh = rz.csfh_time(tt, *par_model)
-    dtd = rz.dtdfunc(tt, *p2)
     pwrl = (-1.0,1.0)
     dud = rz.powerdtd(tt, *pwrl)
-    
-    tmp = convolve(sfh, dtd, 'full')*dt*frac*scale ## now convolved result in forward time
+
+    dtd = rz.dtdfunc(tt, *p0)
+    tmp = convolve(sfh, dtd, 'full')*dt*frac*scale
     rate_fn=tmp[:len(dtd)]
+    junk, m_rates = u.recast(rates[:,0], 0., zz, rate_fn) ## optimized fit
+    (aic, bic)= aic_bic(rates[:,0], rates[:,1], m_rates, 4)
+    print('Optimized fit: aic=%2.1f, bic=%2.1f'%(aic,bic))
+
 
     jdud = convolve(sfh, dud, 'full')*dt*scale*0.065
     jdud = jdud[:len(dtd)]
-
-
-    #from raw rates
-    junk, m_rates = u.recast(rates[:,0], 0., zz, rate_fn) ## optimized fit
-    (aic, bic)= aic_bic(rates[:,0], rates[:,1], m_rates, 4)
-    print(aic, bic)
-
     junk, m_rates = u.recast(rates[:,0], 0., zz, jdud) ## power law
     (aic, bic)= aic_bic(rates[:,0], rates[:,1], m_rates, 2)
-    print(aic, bic)
-    
-    pdb.set_trace()
+    print('power-law fit: aic=%2.1f, bic=%2.1f'%(aic,bic))
 
 
-    clf()
-    ax = subplot(111)
-    ax2 = ax.twinx()
-    ax2.plot(zz, sfh, 'r-', label='CSFH')
-    ax.plot(zz, rate_fn, 'k-', label='Fit', zorder=3)
-    ax.plot(zz, jdud, 'k:', label=r'$t^{%.1f}$'%(pwrl[0]))
-    
-    ax.errorbar(rates[:,0], rates[:,1], yerr=[rates[:,3],rates[:,2]], fmt='o', color='0.6', alpha=0.4)
-    ax.errorbar(brates[:,0], brates[:,1], yerr=[brates[:,3],brates[:,2]],
-                xerr=[brates[:,5],brates[:,4]], fmt='o', color='0.0',zorder=10)
 
-    pwrl = (-1.0,1.0)
-    ax.set_xlim(0,2.6)
-    ax.set_ylim(0,1.8)
-    ax2.set_ylim(0,0.16)
-    
-    ax3 = axes([0.65, 0.62, 0.23, 0.2])
-    ## ax3 = axes([0.55, 0.6, 0.33, 0.25])
-    ax3.plot(tt,log10(dtd),'b-', label= 'Fit')#label='Norm = %1.1f' %(simps(dtd,x=time)))
-    ax3.plot(tt,log10(rz.powerdtd(tt, *pwrl)), 'b:', label=r'$t^{%.1f}$'%(pwrl[0]))
-    ax3.set_ylabel('$\log(\Phi)$')
-    ax3.set_xlabel('Delay Time (Gyr)')
-    ax3.set_xlim(0,12)
-    ax3.set_ylim(-3,0.5)
-    ##ax3.set_ylim(0,1)
-    ## ax3.legend(loc=1,ncol=1,frameon=False, fontsize=8)
-    ax.set_xlabel('Redshift')
-    ax.set_ylabel(r'$10^{-4}$ SNe Ia per year per Mpc$^3$')
-    ax2.set_ylabel(r'M$_{\odot}$ per year per Mpc$^3$')
-    ## ax.set_title(r' $k=%.4f$ M$_{\odot}^{-1}$, $f=%2.1f\%%$' %(scale_k,frac*100))
-    ax.set_title(r'$k=%.4f\,M_{\odot}^{-1},\,\,\varepsilon=%2.1f\%%$' %(scale_k,frac*100))
-    ax.legend(loc=2, frameon=False)
-    ax2.legend(loc=4, frameon=False)
-    
-    savefig(plotname)
-    #return()
+    dtd = rz.dtdfunc(tt, *p1)
+    tmp = convolve(sfh, dtd, 'full')*dt*frac*scale
+    rate_fn=tmp[:len(dtd)]
+    junk, m_rates = u.recast(rates[:,0], 0., zz, rate_fn) ## median CSFH
+    (aic, bic)= aic_bic(rates[:,0], rates[:,1], m_rates, 4)
+    print('Median CSFH fit: aic=%2.1f, bic=%2.1f'%(aic,bic))
+
+    dtd = rz.dtdfunc(tt, *p2)
+    tmp = convolve(sfh, dtd, 'full')*dt*frac*scale
+    rate_fn=tmp[:len(dtd)]
+    junk, m_rates = u.recast(rates[:,0], 0., zz, rate_fn) ## median SFH
+    (aic, bic)= aic_bic(rates[:,0], rates[:,1], m_rates, 4)
+    print('Median SFH fit: aic=%2.1f, bic=%2.1f'%(aic,bic))
