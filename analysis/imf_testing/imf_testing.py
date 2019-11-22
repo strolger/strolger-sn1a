6e49209223340fa7454066b9f70eb72cc77404f5
Lou Strolger <strolger@stsci.edu>
2019-11-22 11:24:07 -0500

    modified:   imf_testing.py 	new file:   junk.png 	modified:   ../../paper/strolger.aux 	modified:   ../../paper/strolger.bbl 	modified:   ../../paper/strolger.bib 	modified:   ../../paper/strolger.blg 	modified:   ../../paper/strolger.log 	modified:   ../../paper/strolger.pdf 	modified:   ../../paper/strolger.tex

--- analysis/imf_testing/imf_testing.py
+++ analysis/imf_testing/imf_testing.py
@@ -20,16 +20,34 @@ def imfs(m1,m2):
 
 
 data = []
-for m1 in arange(2.5,4.1,0.1):
-    for m2 in arange(7,11.0,0.1):
+for m1 in arange(2.0,3.5,0.1):
+    for m2 in arange(6,11.0,0.1):
         data.append(imfs(m1,m2))
 
 data=array(data)
 #print(average(data[:,0]), average(data[:,1]))
-print(std(data[:,0]), std(data[:,1]))
+#print(std(data[:,0]), std(data[:,1]))
 med=quad(imf.salpeter,3,8)[0]/quad(imf.salpeter1,0.1,125)[0]
-print(med)
-print('minus %2.1f%%, plus %2.1f%%' %(std(data[:,0])/med*100., std(data[:,1])/med*100.))
+#print(med)
+#print('minus %2.1f%%, plus %2.1f%%' %(std(data[:,0])/med*100., std(data[:,1])/med*100.))
+N1, bins=histogram(data[:,0])
+N2, bins=histogram(data[:,1],bins=bins)
 
+N1 = N1/sum(N1)
+N2 = N2/sum(N2)
+
+ax=subplot(111)
+ax.plot(bins[:-1],0.5*(N1+N2), 'ko')
+
+yy,xx = zip(*sorted(zip((N1+N2)/2.,bins[:-1])))
+yy = array(yy)
+xx = array(xx)
+intv=0.68
+pdb.set_trace()
+
+idx = where(yy<=intv)
+print(min(xx[idx]), max(xx[idx]))
+savefig('junk.png')
+pdb.set_trace()
 
 
