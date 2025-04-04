a83cce25a5ac4327eb9cf9824fdd544b2bdc8782
strolger <strolger@stsci.edu>
2025-04-04 16:26:47 -0400

    modified:   plot_loglog_dtd.py

--- analysis/DTD_fits/plot_loglog_dtd.py
+++ analysis/DTD_fits/plot_loglog_dtd.py
@@ -49,20 +49,20 @@ ax.fill_between(time, upper, lower, color=color, alpha=0.2)
 ax.plot(time,rz.powerdtd(time, *pwrl, normed=False)*(1.6e-3), '--', color=color, label=r'Field ($\beta={%.2f}^{+0.08}_{-0.07}$)'%(pwrl[0]))
 
 
-color = '0.5'#999933'#F0CBC8'
-pwrl = (-1.25,1.0)
-perr = (0.16, 0.0)
-cov = diag(array(perr)**2)
-ps = np.random.multivariate_normal(pwrl, cov, 100)
-ysample = asarray([rz.powerdtd(time, *pi, normed=False)*(5.75e-3) for pi in ps])
-lower = percentile(ysample, 15.9, axis=0)
-perr = (0.05, 0.0)
-cov = diag(array(perr)**2)
-ps = np.random.multivariate_normal(pwrl, cov, 100)
-ysample = asarray([rz.powerdtd(time, *pi, normed=False)*(5.75e-3) for pi in ps])
-upper = percentile(ysample, 84.1, axis=0)
-ax.fill_between(time, upper, lower, color=color, alpha=0.2)
-ax.plot(time,rz.powerdtd(time, *pwrl, normed=False)*(5.75e-3), '--', lw=2, color=color, label=r'HPK-CL ($\beta={%.1f}^{+0.16}_{-0.15}$)'%(pwrl[0]))
+## color = '0.5'#999933'#F0CBC8'
+## pwrl = (-1.25,1.0)
+## perr = (0.16, 0.0)
+## cov = diag(array(perr)**2)
+## ps = np.random.multivariate_normal(pwrl, cov, 100)
+## ysample = asarray([rz.powerdtd(time, *pi, normed=False)*(5.75e-3) for pi in ps])
+## lower = percentile(ysample, 15.9, axis=0)
+## perr = (0.05, 0.0)
+## cov = diag(array(perr)**2)
+## ps = np.random.multivariate_normal(pwrl, cov, 100)
+## ysample = asarray([rz.powerdtd(time, *pi, normed=False)*(5.75e-3) for pi in ps])
+## upper = percentile(ysample, 84.1, axis=0)
+## ax.fill_between(time, upper, lower, color=color, alpha=0.2)
+## ax.plot(time,rz.powerdtd(time, *pwrl, normed=False)*(5.75e-3), '--', lw=2, color=color, label=r'HPK-CL ($\beta={%.1f}^{+0.16}_{-0.15}$)'%(pwrl[0]))
 
 
 #color = '#AA4499'#F0CBC8'
@@ -87,7 +87,7 @@ idx = where(data[:,6]==1)
 ax2.errorbar(data[idx][:,0], data[idx][:,3]*msc,
             xerr=[-1*data[idx][:,1],data[idx][:,2]],
             yerr=[-1*data[idx][:,4]*msc,data[idx][:,5]*msc],
-            fmt='o', color='k', mfc='1.0', mec='red', label='Cluster Rates')
+            fmt='o', color='k', mfc='1.0', mec='C3', label='Cluster Rates')
 pwrl = (-1.39,1.0)
 tmp=rz.powerdtd(data[idx][:,0], *pwrl, normed=False)*(5.4e-3)
 chi2 = redchisqg(data[idx][:,3]*msc, tmp, sd=data[idx][:,5]*msc)
@@ -104,12 +104,12 @@ idx = where(data[:,6]==0)
 ax2.errorbar(data[idx][1:,0], data[idx][1:,3]*msc,
             xerr=[-1*data[idx][1:,1],data[idx][1:,2]],
             yerr=[-1*data[idx][1:,4]*msc,data[idx][1:,5]*msc],
-            fmt='o', color='k', mfc='1.0', mec='blue', label='Field Rates')
+            fmt='o', color='k', mfc='1.0', mec='C0', label='Field Rates')
 ax2.errorbar(data[idx][0,0], data[idx][0,3]*msc,
              xerr=data[idx][0,2],
              yerr=0.05,#data[idx][0,5]*msc,#,data[idx][:,5]*msc],
              lolims=data[idx][0,5]*msc,
-             fmt='o', color='k', mfc='1.0', marker='^', ms=10, mec='blue', label='LMC & SMC limit')
+             fmt='o', color='k', mfc='1.0', marker='^', ms=10, mec='C0', label='LMC & SMC limit')
 pwrl = (-1.10,1.0)
 tmp=rz.powerdtd(data[idx][1:,0], *pwrl, normed=False)*(1.6e-3)
 chi2 = redchisqg(data[idx][1:,3]*msc, tmp, sd=data[idx][1:,5]*msc, deg=0)
@@ -129,7 +129,6 @@ print(chi2, len(tmp)-1)
 
 popt, pcov = curve_fit(func, data[:,0], data[:,3]*msc, p0=p_t, sigma=data[:,4]*msc)
 print(popt)
-pdb.set_trace()
 popt = [12.921]
 ax.plot(time,dtd*scale*popt,'b-', lw=2, label= 'Exponential model')#label='Norm = %1.1f' %(simps(dtd,x=time)))
 tmp=rz.dtdfunc(data[:,0], *p0)*scale*popt
@@ -162,7 +161,7 @@ for file in files:
         temp = concatenate((temp, samples), axis=0)
     except:
         temp = samples
-
+pdb.set_trace()
 print('%d total samples' %len(temp))
 samples = temp
 ndraws = 100 #21 is actually pretty good
